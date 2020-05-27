21df87c7cee046b39e84926f9333a6ddeb75b090
gaponukz <gaponukz54@hmail.com>
2020-05-27 21:26:38 +0300

    Updates

---
+++ precache-manifest.3304aef945036aae05571f7c56401e24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "840690568e70be032d0d6784dd332370",
+    "url": "/clock/index.html"
+  },
+  {
+    "revision": "08c06ffc9f18d2ea0952",
+    "url": "/clock/static/css/main.a03ef3d2.chunk.css"
+  },
+  {
+    "revision": "6ab5599e911c0494782d",
+    "url": "/clock/static/js/2.d30ac679.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clock/static/js/2.d30ac679.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08c06ffc9f18d2ea0952",
+    "url": "/clock/static/js/main.208d7744.chunk.js"
+  },
+  {
+    "revision": "898503e4b2c3dc799d0d",
+    "url": "/clock/static/js/runtime-main.7a291fb8.js"
+  }
+]);
